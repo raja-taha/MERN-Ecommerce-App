c874432e45f3bcca3d6d6b611e2ed4988335ec75
raja-taha <rajataha062@gmail.com>
2024-08-25 17:14:40 +0500

    Product Page Updated

--- client/src/pages/CategoryPage.jsx
+++ client/src/pages/CategoryPage.jsx
@@ -54,7 +54,7 @@ const CategoryPage = () => {
   return (
     <div className="w-[80%] mx-auto my-5">
       <h2 className="text-[30px] font-poppins font-semibold my-3">
-        {`${capitalizeWords(categoryName)}s`}
+        {`${capitalizeWords(categoryName)}`}
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {products.length === 0 ? (
